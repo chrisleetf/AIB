541d3e69105c7ead889b1069922b122c345aa8a2
chrisleetf <93177962+chrisleetf@users.noreply.github.com>
2021-11-09 13:48:07 +0800

    Add files via upload

---
+++ Install-Applications.ps1
@@ -0,0 +1,65 @@
+# Software install Script
+#
+# Applications to install:
+
+
+#region Set logging 
+$logFile = "c:\temp\" + (get-date -format 'yyyyMMdd') + '_softwareinstall.log'
+function Write-Log {
+    Param($message)
+    Write-Output "$(get-date -format 'yyyyMMdd HH:mm:ss') $message" | Out-File -Encoding utf8 $logFile -Append
+}
+#endregion
+
+#region Chome Installation
+#try {
+#    $LocalTempDir = $env:TEMP; $ChromeInstaller = "ChromeInstaller.exe"; (new-object    System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller"); & "$LocalTempDir\$ChromeInstaller" /silent /install; $Process2Monitor =  "ChromeInstaller"; Do { $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name; If ($ProcessesFound) { "Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2 } else { rm "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose } } Until (!$ProcessesFound)
+#    if (Test-Path "C:\Program Files\Google\Chrome\Application\chrome.exe") {
+#        Write-Log "Google Chrome has been installed"
+#    }
+#    else {
+#        write-log "Error locating the Google Chrome executable"
+#    }
+#}
+#catch {
+#    $ErrorMessage = $_.Exception.message
+#    write-log "Error installing Google Chrome: $ErrorMessage"
+#}
+#endregion
+
+#region Adobe Reader Installation
+try {
+    Start-Process -FilePath "c:\temp\software\AcroRdrDC2100720099_en_US.exe" -ArgumentList "/sPB /rs"
+    if (Test-Path "C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe") {
+        Write-Log "Adobe Reader has been installed"
+    }
+    else {
+        write-log "Error locating the Adobe Reader executable"
+    }
+}
+catch {
+    $ErrorMessage = $_.Exception.message
+    write-log "Error installing Adobe Reader: $ErrorMessage"
+}
+#endregion
+
+#region Install Windows Update
+Powershell.exe -File "C:\temp\Software\InstallWindowUpdateScript.ps1"
+
+#endregion
+
+#region Office365 Installation
+#try {
+#    Start-Process -FilePath "c:\temp\software\Office365\Office365.bat"
+#    if (Test-Path "C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE") {
+#        Write-Log "Office365 has been installed"
+#    }
+#    else {
+#        write-log "Error locating the Office365 executable"
+#    }
+#}
+#catch {
+#    $ErrorMessage = $_.Exception.message
+#    write-log "Error installing Office365: $ErrorMessage"
+#}
+#endregion
